45926a036babfd472839e8c9064d672f3c10475e
Steve Longoria <stevehl25@gmail.com>
2018-03-04 17:30:04 -0800

    Updated listening port in app.js

--- app.js
+++ app.js
@@ -4,10 +4,10 @@
 //1. Create a web server
 
 var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello World\n');
+}).listen(3000, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
 
 //2. Handle HTTP route GET / and POST / i.e. Home
